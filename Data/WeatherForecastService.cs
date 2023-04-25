fb12643e5afca2638b8820ecc28691d7fce16f16
mike3sullivan <mike.3.sullivan@gmail.com>
2023-04-24 22:10:44 -0400

    Add files via upload

---
+++ Data/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorApp.Data;
+
+public class WeatherForecastService
+{
+	private static readonly string[] Summaries = new[]
+	{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	};
+
+	public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+	{
+		string datestring = "2023-04-23";
+		DateTime dateTime = DateTime.MinValue;
+		bool ok = DateTime.TryParse(datestring, out dateTime);
+		
+		return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = startDate.AddDays(index),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		}).ToArray());
+	}
+}
+
+
